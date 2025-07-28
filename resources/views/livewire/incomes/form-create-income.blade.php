1c5e783ca9d3aed06abcfa5a9fa63d952b9591e2
Elliton-Luis <ellitonjunior05@gmail.com>
2025-07-28 12:45:30 -0300

    Fix: change in the form-create-income turning it into a modal

--- resources/views/livewire/incomes/form-create-income.blade.php
+++ resources/views/livewire/incomes/form-create-income.blade.php
@@ -1,4 +1,13 @@
-<div class="space-y-4 w-96 p-6 bg-base-100 shadow-xl rounded-xl" >
+<div class="w-full h-full">
+    <a class="btn btn-ghost w-full h-full justify-start" onclick="modal_create.showModal()">
+        <i class="bi bi-cone-striped"></i>
+            Receita
+    </a>
+
+    <dialog id="modal_create" class="modal" wire:ignore.self>
+        <div class="modal-box w-96 max-w-md flex flex-col items-center">
+            <h3 class="text-2xl font-semibold mb-6 text-center">Cadastrar Receita</h3>
+                <div class="w-96 h-full max-w-md md:max-w-lg mx-auto p-4 sm:p-6 bg-base-100 shadow-xl rounded-xl space-y-4 align-items-center">
     <div>
         @if(Session::has('successIncome'))
             <div role="alert" id="successIncome" class="alert alert-success my-5">
@@ -10,88 +19,85 @@
             <h2 class="text-xl font-bold text-center text-white mb-6">Cadastro de Receita</h2>
 
             <div class="form-control mb-6">
-            <label class="label">
-                <i class="bi bi-pencil-square"></i>
-                <span class="label-text">Descrição</span>
-            </label>
-            <input placeholder="Ex.Salário CLT" type="text" class="input input-bordered" wire:model="description" />
-            </div>
-
-            <div class="form-control mb-6">
-            <label class="label">
-                <i class="bi bi-currency-dollar"></i>
-                <span class="label-text">Valor (R$)</span>
-            </label>
-            <input type="number" step="0.01" class="input input-bordered" wire:model="value" placeholder="Ex: R$ 1.512,00" />
+                <label class="label">
+                    <i class="bi bi-pencil-square mr-2"></i>
+                    <span class="label-text">Descrição</span>
+                </label>
+                <input placeholder="Ex.Salário CLT" type="text" class="input input-bordered w-full" wire:model="description" />
             </div>
 
             <div class="form-control mb-6">
                 <label class="label">
-                    <i class="bi bi-arrow-repeat"></i>
+                    <i class="bi bi-currency-dollar mr-2"></i>
+                    <span class="label-text">Valor (R$)</span>
+                </label>
+                <input type="number" step="0.01" class="input input-bordered w-full" wire:model="value" placeholder="Ex: R$ 1.512,00" />
+            </div>
+
+            <div class="form-control mb-6">
+                <label class="label">
+                    <i class="bi bi-arrow-repeat mr-2"></i>
                     <span class="label-text">Recorrência</span>
                 </label>
-                    <div class="flex gap-2">
-                        <select class="select select-bordered flex-1" wire:model="type">
-                        <option value="null" disabled selected>Selecione um tipo</option>
-                        <option value="sporadic">Esporádico</option>
-                        <option value="recurring">Recorrente</option>
-                        </select>
-                    </div>
+                <select class="select select-bordered w-full" wire:model="type">
+                    <option value="null" disabled selected>Selecione um tipo</option>
+                    <option value="sporadic">Esporádico</option>
+                    <option value="recurring">Recorrente</option>
+                </select>
             </div>
 
-            <div class="form-control">
+            <div class="form-control mb-6">
                 <label class="label">
-                    <i class="bi bi-tags"></i>
+                    <i class="bi bi-tags mr-2"></i>
                     <span class="label-text">Categoria</span>
                 </label>
-                    <div class="flex gap-2">
-                        <select required class="select select-bordered flex-1" wire:model="category">
+                <div class="flex flex-col sm:flex-row gap-2">
+                    <select required class="select select-bordered w-full" wire:model="category">
                         <option value="null" disabled selected>Selecione uma Categoria</option>
                         @forelse ($categories as $category)
                             <option value="{{$category->id}}">{{$category->name}}</option>
                         @empty
                             <option disabled selected>Crie uma Categoria -></option>
                         @endforelse
-                        </select>
-                        <button type="button" class="btn" onclick="my_modal.showModal()">
+                    </select>
+                    <button type="button" class="btn btn-neutral w-full sm:w-auto" onclick="my_modal.showModal()">
                         <i class="bi bi-plus"></i>
-                        </button>
-                    </div>
+                    </button>
+                </div>
             </div>
-
 
             <button type="submit" class="btn btn-primary w-full mt-2">Submit</button>
         </form>
     </div>
 
     <dialog id="my_modal" class="modal">
-        <div class="modal-box p-6 space-y-4 w-80 p-6 bg-base-100 shadow-xl rounded-xl">
-            <h3 class="text-lg font-semibold text-slate-800 mb-4 text-center text-white">Cadastrar Categoria</h3>
+        <div class="modal-box w-full max-w-sm p-6 bg-base-100 shadow-xl rounded-xl space-y-4">
+            <h3 class="text-lg font-semibold text-white text-center">Cadastrar Categoria</h3>
 
-            <div class="mt-6 form-control mb-3">
-            <label class="label">
-                <i class="bi bi-tag text-lg"></i>
-                <span class="label-text text-slate-600 block font-bold text-white">Nome</span>
-            </label>
-            <input placeholder="Ex.Alimentação" type="text" class="input input-bordered border-slate-500 focus:border-primary focus:ring-0" wire:model="categoryName" />
+            <div class="form-control mb-3">
+                <label class="label">
+                    <i class="bi bi-tag mr-2"></i>
+                    <span class="label-text text-white font-bold">Nome</span>
+                </label>
+                <input placeholder="Ex.Alimentação" type="text" class="input input-bordered" wire:model="categoryName" />
             </div>
 
             <div class="form-control mb-4">
-            <label class="label">
-                <i class="bi bi-arrow-down-circle text-lg"></i>
-                <span class="label-text text-slate-600 font-bold text-white">Tipo</span>
-            </label>
-            <select class="select select-bordered border-slate-500 focus:border-primary focus:ring-0" wire:model="categoryType">
-                <option value="null" disabled selected>Selecione o Tipo da Categoria</option>
-                <option value="income">Receita</option>
-                <option value="expense">Despesa</option>
-                <option value="both">Ambos</option>
-            </select>
+                <label class="label">
+                    <i class="bi bi-arrow-down-circle mr-2"></i>
+                    <span class="label-text text-white font-bold">Tipo</span>
+                </label>
+                <select class="select select-bordered" wire:model="categoryType">
+                    <option value="null" disabled selected>Selecione o Tipo da Categoria</option>
+                    <option value="income">Receita</option>
+                    <option value="expense">Despesa</option>
+                    <option value="both">Ambos</option>
+                </select>
             </div>
 
-            <div class="modal-action mt-6">
-                <button class="btn bg-red-500 shadow-lg" onclick="document.getElementById('my_modal').close()">Fechar</button>
-                <button class="btn bg-teal-700 shadow-lg" wire:click="storeCategory()" onclick="document.getElementById('my_modal').close()">Salvar</button>
+            <div class="modal-action justify-end gap-2">
+                <button class="btn btn-error" onclick="document.getElementById('my_modal').close()">Fechar</button>
+                <button class="btn btn-success" wire:click="storeCategory()" onclick="document.getElementById('my_modal').close()">Salvar</button>
             </div>
         </div>
     </dialog>
