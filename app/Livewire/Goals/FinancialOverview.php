fcce03c7557455d6fda9affa7b35b15c4953d157
Elliton-Luis <ellitonjunior05@gmail.com>
2025-09-05 19:54:35 -0300

    Feature: create goals index page

---
+++ app/Livewire/Goals/FinancialOverview.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Goals;
+
+use Livewire\Component;
+
+class FinancialOverview extends Component
+{
+    public function render()
+    {
+        return view('livewire.goals.financial-overview');
+    }
+}
