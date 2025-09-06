0a762e832895ab7d92bb216077ba1fbde4a18608
Elliton-Luis <ellitonjunior05@gmail.com>
2025-09-06 20:29:05 -0300

    Feature: create goal model

---
+++ app/Models/Goal.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Goal extends Model
+{
+    protected $fillable = [
+        'user_id', 'name', 'total_amount', 'current_amount',
+        'status', 'deadline', 'priority', 'icon_id'
+    ];
+
+    public function icon()
+    {
+        return $this->belongsTo(Icon::class);
+    }
+}
