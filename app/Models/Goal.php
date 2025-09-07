1160508f39f86f3b5191c06a8a2274df05326f99
Elliton-Luis <ellitonjunior05@gmail.com>
2025-09-06 22:12:38 -0300

    Feature: create goal model

--- app/Models/Goal.php
+++ app/Models/Goal.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Goal extends Model
 {
+    protected $table = 'goals';
+
     protected $fillable = [
         'user_id', 'name', 'total_amount', 'current_amount',
         'status', 'deadline', 'priority', 'icon_id'
