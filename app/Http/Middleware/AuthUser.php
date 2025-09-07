d90754a147b578fa1f466b561c232a9081dcbcde
Elliton-Luis <ellitonjunior05@gmail.com>
2025-09-07 11:27:03 -0300

    Feature: add auth function middleware

--- app/Http/Middleware/AuthUser.php
+++ app/Http/Middleware/AuthUser.php
@@ -5,11 +5,15 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AuthUser
 {
-    public function handle(Request $request, Closure $next): Response
+     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if(Auth::check()){
+            return $next($request);
+        }
+        return redirect()->route('login.index')->with('error','Você deve estar logado para acessar essa área');
     }
 }
