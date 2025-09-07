1e5227e9dabfa8eed1e3234206ef41a92a5ec710
Elliton-Luis <ellitonjunior05@gmail.com>
2025-09-07 11:28:21 -0300

    Fix: organize routes and add middleware

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\IncomeController;
 use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\GoalsController;
 
+use App\Http\Middleware\AuthUser;
+
 Route::get('/', function () {
     return view('login.index');
 })->name('login.index');
 
-Route::get('/lobby', function (){
-    return view('lobby');
-})->name('lobby');
+Route::post('/auth', [LoginController::class, 'authUser'])->name('login.auth');
 
-Route::post('/auth', [LoginController::class,'authUser'])->name('login.auth');
-Route::get('/logout', [LoginController::class,'logoutUser'])->name('login.logout');
+Route::middleware([AuthUser::class])->group(function () {
 
-Route::prefix('/receitas')->group( function(){
-    Route::get('/',[IncomeController::class,'showIncome'])->name('income.index');
+    Route::get('/logout', [LoginController::class, 'logoutUser'])->name('login.logout');
+
+    Route::get('/lobby', function () {
+        return view('lobby');
+    })->name('lobby');
+
+    Route::prefix('/receitas')->group(function () {
+        Route::get('/', [IncomeController::class, 'showIncome'])->name('income.index');
+    });
+
+    Route::prefix('/despesas')->group(function () {
+        Route::get('/', [ExpenseController::class, 'showExpense'])->name('expense.index');
+    });
+
+    Route::get('/metas', [GoalsController::class, 'showGoals'])->name('goals.index');
+
 });
-
-Route::prefix('/despesas')->group( function(){
-    Route::get('/',[ExpenseController::class,'showExpense'])->name('expense.index');
-});
-
-Route::get('/metas', [GoalsController::class,'showGoals'])->name('goals.index');
-
-
-
-
