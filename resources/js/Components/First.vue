bfb2d06abf284f9f6aed8c9dd4beb9352519f7dd
Lucas Gabriel <lucasfbr123@gmail.com>
2025-09-11 23:06:23 -0300

    Feature: add vuejs

---
+++ resources/js/Components/First.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full p-5 flex justify-center">
+        <button @click="counter++" class="w-1/3 btn btn-warning">
+            {{ counter }}
+        </button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const counter = ref(0);
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+const files = import.meta.glob('./Components/**/*.vue', {eager: true});
+
+Object.entries(files).forEach(([path, definition]) => {
+    const name = path.split('/').pop().replace(/\.\w+$/,'');
+
+    app.component(name, definition.default);
+});
+
+app.mount('#app');
